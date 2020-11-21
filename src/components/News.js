180eabb61d250388b0384ceb39c36954e54c0390
Amir <amir@musaev.online>
2020-11-21 15:28:08 +0000

    refactor

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import './Home.css';
 
 class Home extends React.Component {
     render() {
--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ import './index.css';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Suspense fallback={<div>Загрузка...</div>}>
+      <Suspense fallback={<div >Загрузка...</div>}>
         <BrowserRouter>
-          <Route path="/" exact component={React.lazy(() => import('./Home'))} />
-          <Route path="/:id" exact component={React.lazy(() => import('./News'))} />
+          <Route path="/" exact component={React.lazy(() => import('./components/Home'))} />
+          <Route path="/:id" exact component={React.lazy(() => import('./components/News'))} />
         </BrowserRouter>
       </Suspense>
     </Provider>
